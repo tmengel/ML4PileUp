87ced413ed5b6ad828a34a0d575f9849d0917b39
tmengel <tmengel@vols.utk.edu>
2024-03-27 18:34:02 -0400

    new makefile

--- network-pruning/CutDataFile.cpp
+++ network-pruning/CutDataFile.cpp
@@ -1,44 +1,64 @@
-#include "TFile.h"
-#include "TTree.h"
-#include "TString.h"
-#include "TMath.h"
-
 #include <iostream>
 #include <vector>
+#include <string>
+#include <cstdlib>
 
-using namespace std;
-using namespace ROOT;
+#include <TFile.h>
+#include <TTree.h>
+#include <TString.h>
+#include <TMath.h>
+
 
 int main(int argc, char** argv)
-{
-    TString inputfile = "/lustre/isaac/scratch/tmengel/ML4PileUp/data/DataSmall.root";
-    std::cout << "inputfile: " << inputfile.Data() << std::endl;
-
-    TString treename = "OutputTree";
-    std::cout << "treename: " << treename.Data() << std::endl;
-
-    // open file
-    TFile fin(inputfile.Data(), "READ");
-    if(!fin.IsOpen()){
-        std::cout << "Error: could not open file " << inputfile << std::endl;
+{   
+    if(argc != 2)
+    {
+        std::cout << "Usage: CutDataFile <inputfile>" << std::endl;
         return 1;
     }
-    TTree * tree = (TTree*)fin.Get(treename.Data());
+    
+    std::string inputfile = argv[1];
+    std::string outputfile = "DataSmallFloat.root";
+    if(argc == 3) outputfile = argv[2];
+    float percentage = 0.1;
+    if(argc == 4) percentage = atof(argv[3]);
+
+    bool pile_up_only = false;
+    if(argc == 5) pile_up_only = atoi(argv[4]) == 1 ? true : false;
+
+    // print input
+    std::cout << "inputfile: " << inputfile << std::endl;
+    std::cout << "outputfile: " << outputfile << std::endl;
+    std::cout << "percentage: " << percentage << std::endl;
+    std::cout << "pile_up_only: " << pile_up_only << std::endl;
+
+    // tree name
+    std::string treename = "OutputTree";
+
+    // open file
+    TFile * fin = new TFile(inputfile.c_str(), "READ");
+    if(!fin->IsOpen()){
+        std::cout << "Error: could not open file " << inputfile << std::endl;
+        exit(1); // failed
+    }
+
+    TTree * tree = (TTree*)fin->Get(treename.c_str());
     if(!tree){
         std::cout << "Error: could not find tree in file " << inputfile << std::endl;
-        return 1;
+        exit(1); // failed
     }
 
     // input variables
+    bool pileup = false;
     double amp = 0;
     double phase = 0;
     std::vector<double> * trace = 0;
 
     // set branch addresses
+    tree->SetBranchAddress("pileup",&pileup);
     tree->SetBranchAddress("amp",&amp);
     tree->SetBranchAddress("phase",&phase);
     tree->SetBranchAddress("trace",&trace);
-
     int nentries = tree->GetEntries();
 
     // output variables
@@ -49,49 +69,52 @@ int main(int argc, char** argv)
 
 
     // output file
-    TString outputfile = "DataSmallFloat.root";
-    TFile * fout = new TFile(outputfile.Data(), "RECREATE");
+    TFile * fout = new TFile(outputfile.c_str(), "RECREATE");
     TTree * tree_out = new TTree("OutputTree", "OutputTree");
-    // tree_out->Branch("pile", &pile_out);
+    tree_out->Branch("pileup", &pile_out);
     tree_out->Branch("amp", &amp_out);
     tree_out->Branch("phase", &phase_out);
     tree_out->Branch("trace", &trace_out);
 
-    // loop over events
-    // int nEventsToSave = 10000;
     int events_saved = 0;
-    for(int i = 0; i < nentries; i++){
-        
-        if(i % 10 != 0) continue;
-        tree->GetEntry(i);
+    int inverted_percentage = int(1.0/percentage);
 
+    for(int i = 0; i < nentries; i++)
+    {
+        
+        if(i % inverted_percentage != 0) continue; // skip events
         if(i % 1000000 == 0) std::cout << "Event " << i << " of " << nentries << std::endl;
 
+        tree->GetEntry(i);
+
         // only save events with no pileup
-        // if (phase == 0.0) continue;
-        // events_saved++;
+        if(pile_up_only && !pileup) continue;
+    
         amp_out = amp;
         phase_out = phase;
-        // pile_out = 0;
+        pile_out = pileup ? 1 : 0;
         trace_out.clear();
-        for(int j = 0; j < trace->size(); j++){
+
+        for(int j = 0; j < trace->size(); j++)
+        {
             trace_out.push_back(trace->at(j));
         }
 
         tree_out->Fill();
-
-        // if(i > nEventsToSave) break;
+        events_saved++;
     }
+
+    std::cout << "Events saved: " << events_saved << std::endl;
 
     fout->Write();
     fout->Close();
 
     // clean up
-    fin.Close();
+    fin->Close();
 
     std::cout << "Done!" << std::endl;
 
 
     return 0;
-
+   
 }
